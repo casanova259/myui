e9149b823bb31e81850035a7de2a7a55c031fdd4
Manik Sharma <casanova270407@gmail.com>
2026-05-14 15:55:57 +0530

    started working on the mask

---
+++ app/mask/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Mask } from "@/components/mask/mask";
+
+export default function Page() {
+    return <>
+        <Mask />
+    </>
+}
---
+++ components/mask/mask.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+
+
+export const Mask = () => {
+    return (
+        <main className="min-h-screen">
+
+            {/* Mask */}
+            <div className="h-full w-full flex items-center justify-center text-[#bc9f79] text-6xl cursor-default ">
+                I m a engineer who loves to Build and challange myself with new ideas.
+                (yet) i m making good stuff and build myself .
+            </div>
+
+
+            {/* Body */}
+            <div className="h-full w-full flex items-center justify-center text-[#afa18f] text-6xl cursor-default">
+                <p className="w-full">
+                    I am a <span className="text-red-500">  selectively skilled engineer</span> who loves to build some dope s**t and producing high quality and
+                    amazing products
+                </p>
+            </div>
+        </main>
+    );
+};
