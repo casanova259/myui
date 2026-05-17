78d84f3dab9ef496a6694788910e3e63132298b5
Manik Sharma <casanova270407@gmail.com>
2026-05-17 23:48:22 +0530

    made it but understand it later

---
+++ app/encrypt/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Example from "@/components/encrypt/ecprty";
+
+export default function Page() {
+    return <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <Example />
+
+    </div>
+}
