655370028c353e62194a4870b3b6cc3d12f51bd6
Manik Sharma <casanova270407@gmail.com>
2026-05-18 15:13:38 +0530

    one more

--- components/SmoothScroll/lenis.tsx
+++ components/SmoothScroll/lenis.tsx
@@ -2,28 +2,32 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useEffect, useRef } from 'react';
-import { ReactLenis } from 'lenis/react';
+import { ReactLenis, LenisRef } from 'lenis/react';
 import 'lenis/dist/lenis.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const LenisSmoothScroll = () => {
-    const lenisRef = useRef(null);
+    const lenisRef = useRef<LenisRef>(null);
 
     useEffect(() => {
-        function update(time) {
+        function update(time: number) {
             lenisRef.current?.lenis?.raf(time * 1000)
         }
 
         gsap.ticker.add(update)
-
-        // Refresh ScrollTrigger on mount (handles route changes)
         ScrollTrigger.refresh();
 
         return () => gsap.ticker.remove(update)
     }, []);
 
-    return <ReactLenis root options={{ autoRaf: false, duration: 1.2, touchMultiplier: 2, smoothTouch: true }} ref={lenisRef} />
+    return (
+        <ReactLenis
+            root
+            options={{ autoRaf: false, duration: 1.2, touchMultiplier: 2 }}
+            ref={lenisRef}
+        />
+    );
 }
 
 export default LenisSmoothScroll;
