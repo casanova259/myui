2c3115fd62d62b3bb145d052a7b6b54bcd00f29a
Manik Sharma <casanova270407@gmail.com>
2026-05-18 14:59:30 +0530

    again

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-
-import { Content } from "@/components/content";
+"use client";
+// import { Content } from "@/components/content";
 
 export default function Page() {
-  return <Content />;
+  return <main className="min-h-screen flex items-center justify-center bg-black">
+    <h2 className="text-neutral-300">Landing Page Soon.....</h2>
+  </main>;
 }
