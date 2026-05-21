6fe059a8b22a2c20ecf28fbda6740e09aea2fb37
Manik Sharma <casanova270407@gmail.com>
2026-05-21 11:16:46 +0530

    Done with the last one completed

--- app/pinscroll/page.tsx
+++ app/pinscroll/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import SectionItoshiRin, { SectionNagi, SectionShiedo } from "@/components/Pincards/PinCards";
+import SectionItoshiRin, { SectionBaro, SectionNagi, SectionShiedo } from "@/components/Pincards/PinCards";
 import { SectionIsagi } from "../../components/Pincards/PinCards";
 
 import Image from "next/image";
@@ -10,9 +10,9 @@ import ScrollAnimation from "@/components/Pincards/ScrollAnimation";
 // app/page.tsx
 export default function Home() {
     return (
-        <main className="w-full bg-[#0f0f0f]">
+        <main className="w-full bg-[#0f0f0f] overflow-x-hidden">
             <ScrollAnimation/>
-            <header className="relative flex h-[100svh] w-full items-center justify-center bg-black p-8 text-white">
+            <header className="relative flex h-[100svh] w-full items-center justify-center bg-black p-8 text-white ">
                 <h1 className="text-center text-[9rem] leading-none font-black tracking-tighter uppercase font-monster-beast">
                     Meet The Egoists <br />
                     <Image
@@ -26,9 +26,10 @@ export default function Home() {
                 </h1>
             </header>
             <SectionItoshiRin />
-            <SectionShiedo />
-            <SectionIsagi />
             <SectionNagi />
+            <SectionIsagi />
+            <SectionShiedo />
+            <SectionBaro/>
             <footer className="relative flex h-svh w-full items-center justify-center bg-[#0f0f0f] p-8 text-white">
                 <h1 className="text-center text-[clamp(3rem,10vw,8rem)] leading-none font-black tracking-tighter uppercase font-monster-beast">
                     IT IsN'T Over  <br />yet
--- components/Pincards/PinCards.tsx
+++ components/Pincards/PinCards.tsx
@@ -230,7 +230,7 @@ export const SectionIsagi = () => {
                             <span className="text-[#27303C]">YOICHI</span>
                         </h1>
                         <p className="max-w-xl text-[1.25rem] leading-tight font-medium text-neutral-900 md:text-[1.65rem] font-monster-beast">
-                            Hey genius, is a lifeform like me not enough of a reason for you to fight?
+                            So this is what it means to win. I don’t know what this feeling is… but I like it!”
                         </p>
                         <div className="mt-2 border border-black/30 bg-white px-3 py-1">
                             <span className="text-xs font-bold tracking-widest text-black/80 uppercase font-monster-beast">
@@ -273,6 +273,102 @@ export const SectionIsagi = () => {
                             <div className="absolute -bottom-8 left-1/2 z-20 -translate-x-1/2 bg-black px-4 py-0.5 whitespace-nowrap">
                                 <span className="font-mono text-[11px] font-bold tracking-[0.3em] text-white uppercase">
                                     METAVISION / DIRECT SHOT
+                                </span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    );
+};
+export const SectionBaro = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const handleMouseEnter = () => {
+        if (videoRef.current) {
+            videoRef.current.muted = false;
+            videoRef.current.volume = 1;
+        }
+    };
+
+    const handleMouseLeave = () => {
+        if (videoRef.current) {
+            videoRef.current.muted = true;
+        }
+    };
+
+    return (
+        <section data-section className="relative h-svh min-h-svh  w-full overflow-hidden flex items-center justify-center">
+
+            <div className="absolute bg-[#F6DC0E] flex items-center justify-center h-full w-full">
+
+                {/* watermark */}
+                <div className="absolute inset-0 z-0 opacity-[0.20] pointer-events-none flex items-center justify-center">
+                    <span className="text-[40svh] leading-none font-black whitespace-nowrap text-red-500 font-monster-beast">
+                        KING
+                    </span>
+                </div>
+
+                {/* content */}
+                <div className="relative z-10 flex flex-col md:flex-row gap-64 h-[80vh]">
+
+                    {/* LEFT — text */}
+                    <div className="flex flex-1 flex-col items-start justify-center gap-4 pb-6 md:pb-8 mt-8">
+                        <span className="text-sm font-bold text-red-700 uppercase tracking-widest font-monster-beast">
+                            KING
+                        </span>
+                        <h1 className="mt-2 text-[clamp(4rem,12vw,14rem)] leading-[0.85] font-black tracking-tighter uppercase text-red-500 font-monster-beast">
+                            Shoei  <br />
+                            <span className="text-[#1d1717]">BARO</span>
+                        </h1>
+                        <p className="max-w-xl text-[1.25rem] leading-tight font-medium text-black md:text-[1.65rem] font-monster-beast">
+                            While you stood in the light, I was the shadow you didn't see. Now it's time for the darkness to devour you!
+                        </p>
+                        <div className="mt-2 border border-black/30 bg-red-500 px-3 py-1">
+                            <span className="text-xs font-bold tracking-widest text-yellow-400 uppercase font-monster-beast">
+                                KING — BLUELOCK XI
+                            </span>
+                        </div>
+                    </div>
+
+                    {/* RIGHT — video frame */}
+                    <div className="order-1 flex flex-1 items-center justify-center pt-12 md:order-2 md:pt-0">
+                        <div className="relative h-[50vh] w-[20vw] p-4 md:w-[25vw]">
+
+                            {/* two decorative offset borders */}
+                            <div className="absolute inset-0 z-0 rotate-3 border border-dashed border-black opacity-60" />
+                            <div className="absolute inset-0 z-0 -rotate-2 border-[1.5px] border-black opacity-100 bg-black" />
+
+                            {/* main video box */}
+                            <div
+                                className="relative z-10 h-full w-full overflow-hidden border-2 border-white bg-[#7a8074] outline outline-black/90"
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                            >
+                                <video
+                                    ref={videoRef}
+                                    src="/videos/BARO.mp4"
+                                    autoPlay muted loop playsInline
+                                    className="h-full w-full object-cover transition-transform duration-500 hover:scale-105"
+                                />
+                            </div>
+
+                            {/* corner brackets — top left */}
+                            <div className="absolute top-2 left-2 z-20 h-10 w-10 border-t-4 border-l-4 border-black" />
+                            <div className="absolute top-2 left-2 z-20 h-14 w-14 border-t border-l border-white/70" />
+
+                            {/* corner brackets — bottom right */}
+                            <div className="absolute right-2 bottom-2 z-20 h-10 w-10 border-r-4 border-b-4 border-black" />
+                            <div className="absolute right-2 bottom-2 z-20 h-14 w-14 border-r border-b border-white/70" />
+
+                            {/* bottom label */}
+                            <div className="absolute -bottom-8 left-1/2 z-20 -translate-x-1/2 bg-red-500 px-4 py-0.5 whitespace-nowrap">
+                                <span className="font-mono text-[11px] font-bold tracking-[0.3em] text-white uppercase">
+                                    KING
                                 </span>
                             </div>
 
