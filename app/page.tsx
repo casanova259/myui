c0db60bd8b33783a2aced4abf74afb2161ed7948
Manik Sharma <casanova270407@gmail.com>
2026-06-19 22:53:12 +0530

    just changed the font

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { Structure } from "./_components/Structure";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+
 export default function Page() {
-  return <main className="min-h-screen flex items-center justify-center bg-black">
+  return <main className="min-h-screen flex items-center justify-center bg-black font-inter">
     <Structure />
   </main>;
 }
